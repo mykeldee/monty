aa0b108c1e0200db94da6b6741f26643e82065ab
Michael Dankyira <kdankyira.education@gmail.com>
2022-09-08 20:10:25 -0700

    Update Files

--- monty.h
+++ monty.h
@@ -2,15 +2,37 @@
 #define __MONTY_H__
 
 #include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <ctype.h>
 
-#define STACK 0
+
 #define QUEUE 1
-#define DELIMS " \n\t\a\b"
+#define STACK 0
+#define DELIM " \n\t\a\b"
 
-/* GLOBAL OPCODE TOKENS */
-extern char **op_toks;
+/**
+ * globales_s - struct with variable globals
+ * @token2: integer
+ * @fd: points to the previous element of the stack (or queue)
+ * @line_buf: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct globales_s
+{
+	char *token2;
+	FILE *fd;
+	char *line_buf;
+
+} globales_t;
+
+extern globales_t globalvar;
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
@@ -42,48 +64,77 @@ typedef struct instruction_s
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-/* PRIMARY INTERPRETER FUNCTIONS */
-void free_stack(stack_t **stack);
-int init_stack(stack_t **stack);
-int check_mode(stack_t *stack);
-void free_tokens(void);
-unsigned int token_arr_len(void);
-int run_monty(FILE *script_fd);
-void set_op_tok_error(int error_code);
+/* main.c */
+int main(int argc, char **argv);
 
-/* OPCODE FUNCTIONS */
-void monty_push(stack_t **stack, unsigned int line_number);
-void monty_pall(stack_t **stack, unsigned int line_number);
-void monty_pint(stack_t **stack, unsigned int line_number);
-void monty_pop(stack_t **stack, unsigned int line_number);
-void monty_swap(stack_t **stack, unsigned int line_number);
-void monty_add(stack_t **stack, unsigned int line_number);
-void monty_nop(stack_t **stack, unsigned int line_number);
-void monty_sub(stack_t **stack, unsigned int line_number);
-void monty_div(stack_t **stack, unsigned int line_number);
-void monty_mul(stack_t **stack, unsigned int line_number);
-void monty_mod(stack_t **stack, unsigned int line_number);
-void monty_pchar(stack_t **stack, unsigned int line_number);
-void monty_pstr(stack_t **stack, unsigned int line_number);
-void monty_rotl(stack_t **stack, unsigned int line_number);
-void monty_rotr(stack_t **stack, unsigned int line_number);
-void monty_stack(stack_t **stack, unsigned int line_number);
-void monty_queue(stack_t **stack, unsigned int line_number);
+/* node functions */
+stack_t *create_node_stackfirst(stack_t **stack, int n);
+stack_t *create_node_stackend(stack_t **stack, int n);
+void free_node_stack(stack_t **stack);
 
-/* CUSTOM STANDARD LIBRARY FUNCTIONS */
-char **strtow(char *str, char *delims);
-char *get_int(int n);
+/* get_builtin */
+int get_builtin(char *token, stack_t **stack, unsigned int line_number);
+/* builtins functions */
+void push(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
+void add(stack_t **stack, unsigned int line_number);
+void nop(stack_t **stack, unsigned int line_number);
+void sub(stack_t **stack, unsigned int line_number);
+void pint(stack_t **stack, unsigned int line_number);
+void swap(stack_t **stack, unsigned int line_number);
+void pchar(stack_t **stack, unsigned int line_number);
+void mul(stack_t **stack, unsigned int line_number);
+void div_m(stack_t **stack, unsigned int line_number);
+void mod_m(stack_t **stack, unsigned int line_number);
+void pstr_t(stack_t **stack, unsigned int line_number);
+void rotrl(stack_t **stack, unsigned int line_number);
 
-/* ERROR MESSAGES & ERROR CODES */
-int usage_error(void);
-int malloc_error(void);
-int f_open_error(char *filename);
-int unknown_op_error(char *opcode, unsigned int line_number);
-int no_int_error(unsigned int line_number);
-int pop_error(unsigned int line_number);
-int pint_error(unsigned int line_number);
-int short_stack_error(unsigned int line_number, char *op);
-int div_error(unsigned int line_number);
-int pchar_error(unsigned int line_number, char *message);
 
-#endif /* __MONTY_H__ */
+
+
+
+/*error*/
+void stderr_usage(void);
+void stderr_malloc(void);
+void stderr_fopen(char *fd);
+void stderr_int(unsigned int line_number);
+void stderr_unknown(char *token, unsigned int line_number);
+void div_e(unsigned int line_number);
+void op_e(unsigned int line_number, char *op);
+/* TEST F */
+
+void tokerr(int error_code);
+unsigned int array_len(void);
+char *get_int(int num);
+unsigned int abs_m(int);
+int len_buff_uint(unsigned int num, unsigned int base);
+void full_buff(unsigned int num, unsigned int base, char *buff, int buff_size);
+void pop_e(unsigned int line_number);
+void pint_e(unsigned int line_number);
+void pchar_e(unsigned int line_number, char *msg);
+int _isdigit();
+void free_dlistint(stack_t *stack);
+void free_globalvars(void);
+void rotr(stack_t **stack, unsigned int line_number);
+stack_t *add_dnodeint(stack_t **head, int n);
+
+/* UNUSED F
+void pop(stack_t **stack, unsigned int line_number);
+void pint(stack_t **stack, unsigned int line_number);
+int cmd_monty(FILE *fd);
+void free_tok(void);
+int check_delim(char stream, char *delim);
+int wstrlen(char *str, char *delim);
+int wcounter(char *str, char *delim);
+char **strtow(char *str, char *delim);
+char *wnext(char *str, char *delim);
+int create_node_stack(stack_t **stack);
+void free_node_stack(stack_t **stack);
+int no_line(char *line, char *delim);
+int check_opcode(stack_t *stack);
+*/
+
+
+
+#endif
